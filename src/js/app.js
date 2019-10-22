0de59d7b396a426ec9eca6cccd767f713bdd37d9
HeysemLNU <ab224hv@student.lnu>
2019-10-22 15:03:27 +0200

    now can save scores

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,54 @@
 const submitButton = document.getElementById('button')
 let totalTime = 0
 const userName = document.querySelector('#nameOfUser')
+let userNameString = ''
 const urlLink = 'http://vhost3.lnu.se:20080/question/1'
 let willYouCountinue = false
 let givenAnswer = null
 let recievedObj = {}
+const finalScore = 0
 function removeNameEntering () {
   const divMain = document.querySelector('#inputName')
   const toRemove = document.querySelector('#toBeRemoved')
   divMain.removeChild(toRemove)
 }
+function saveScore () {
+  const scoreObj = {
+    name: userNameString,
+    time: totalTime,
+    score: finalScore
+  }
+  localStorage.setItem(scoreObj.name, JSON.stringify(scoreObj))
+}
+function showScore () {
+  const theBody = document.querySelector('body')
+  for (let i = 0; i < localStorage.length; i++) {
+    const playerScore = document.createElement('li')
+    console.log(JSON.parse(localStorage.getItem(localStorage.key(i))))
+    playerScore.textContent = JSON.parse(localStorage.getItem(localStorage.key(i))).name + ' ' + JSON.parse(localStorage.getItem(localStorage.key(i))).time
+    theBody.appendChild(playerScore)
+  }
+}
 submitButton.addEventListener('click', function () {
+  userNameString = document.querySelector('#usernameInput').value
   userName.textContent = document.querySelector('#usernameInput').value
   removeNameEntering()
   window.fetch(urlLink).then(responseFromServer => {
     responseFromServer.json().then(finalParse => { console.log(finalParse); recievedObj = finalParse }).then(() => {
       const theBody = document.querySelector('body')
+      const divMain = document.querySelector('#inputName')
       const questionField = document.createElement('div')
       const questionShow = document.createTextNode(recievedObj.question)
       questionField.appendChild(questionShow)
-      theBody.appendChild(questionField)
+      divMain.appendChild(questionField)
       const answerInput = document.createElement('input')
       answerInput.type = 'text'
-      theBody.appendChild(answerInput)
+      divMain.appendChild(answerInput)
       const answerButton = document.createElement('input')
       answerButton.id = 'answerSubmit'
       answerButton.type = 'submit'
       answerButton.value = 'Send'
-      theBody.appendChild(answerButton)
+      divMain.appendChild(answerButton)
       let countDown = 0
       const timer = setInterval(() => {
         console.log(countDown)
@@ -37,17 +58,17 @@ submitButton.addEventListener('click', function () {
           totalTime += countDown
           clearInterval(timer)
           console.log('No you didnt')
+          theBody.removeChild(divMain)
+          showScore()
         }
       }, 1000)
       answerButton.addEventListener('click', () => {
         givenAnswer = answerInput.value
         willYouCountinue = true
         totalTime += countDown
-        countDown = 20
+        console.log(totalTime)
         clearInterval(timer)
-        console.log('yes you did')
       })
-      console.log('you came this far')
     })
   })
 })
